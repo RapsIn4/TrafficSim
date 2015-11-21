c43be5d4efc873a03061abb437b2fb8b952806ae
Michael Ye <m6ye@uwaterloo.ca>
2015-11-21 16:20:29 -0600

    save work

--- Frontend/assets/javascript/main/main.js
+++ Frontend/assets/javascript/main/main.js
@@ -1,32 +1,130 @@
-// create a renderer instance width=640 height=480
-var renderer = PIXI.autoDetectRenderer(640,480);
+var SCREEN_HEIGHT = screen.height;
+var SCREEN_WIDTH = screen.width;
 
-// create an empty container
-var gameContainer = new PIXI.Graphics();
-   // add the renderer view element to the DOM
+var CAR_WIDTH = 4;
+var LANE_WIDTH = 8;
+
+var renderer = PIXI.autoDetectRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, { antialias: true });
 document.body.appendChild(renderer.view);
 
-// set background color grey
-renderer.backgroundColor = 0x888888;
+// create the root of the scene graph
+var stage = new PIXI.Container();
 
-loadCircles();
+stage.interactive = true;
 
-function loadCircles(){
-  gameContainer.beginFill(255,1);
-    gameContainer.drawCircle(10, 10, 10);
-  gameContainer.endFill();
+// var graphics = new PIXI.Graphics();
 
-  requestAnimationFrame(animate);
+// // set a fill and line style
+// graphics.beginFill(0xFF3300);
+// graphics.lineStyle(10, 0xffd900, 1);
+
+// // draw a shape
+// graphics.moveTo(50,50);
+// graphics.lineTo(250, 50);
+// graphics.lineTo(100, 100);
+// graphics.lineTo(250, 220);
+// graphics.lineTo(50, 220);
+// graphics.lineTo(50, 50);
+// graphics.endFill();
+
+// // set a fill and line style again
+// graphics.lineStyle(10, 0xFF0000, 0.8);
+// graphics.beginFill(0xFF700B, 1);
+
+// // draw a second shape
+// graphics.moveTo(210,300);
+// graphics.lineTo(450,320);
+// graphics.lineTo(570,350);
+// graphics.quadraticCurveTo(600, 0, 480,100);
+// graphics.lineTo(330,120);
+// graphics.lineTo(410,200);
+// graphics.lineTo(210,300);
+// graphics.endFill();
+
+// // draw a rectangle
+// graphics.lineStyle(2, 0x0000FF, 1);
+// graphics.drawRect(50, 250, 100, 100);
+
+// // draw a circle
+// graphics.lineStyle(0);
+// graphics.beginFill(0xFFFF0B, 0.5);
+// graphics.drawCircle(470, 200,100);
+// graphics.endFill();
+
+// graphics.lineStyle(20, 0x33FF00);
+// graphics.moveTo(30,30);
+// graphics.lineTo(600, 300);
+
+
+// stage.addChild(graphics);
+
+// let's create a moving shape
+var thing = new PIXI.Graphics();
+stage.addChild(thing);
+thing.position.x = SCREEN_WIDTH/2;
+thing.position.y = SCREEN_HEIGHT/2;
+
+var count = 0;
+
+// // Just click on the stage to draw random lines
+// stage.on('click', onClick);
+// stage.on('tap', onClick);
+
+// function onClick()
+// {
+//     graphics.lineStyle(Math.random() * 30, Math.random() * 0xFFFFFF, 1);
+//     graphics.moveTo(Math.random() * SCREEN_WIDTH,Math.random() * SCREEN_HEIGHT);
+//     graphics.bezierCurveTo(Math.random() * SCREEN_WIDTH,Math.random() * SCREEN_HEIGHT,
+//                             Math.random() * SCREEN_WIDTH,Math.random() * SCREEN_HEIGHT,
+//                             Math.random() * SCREEN_WIDTH,Math.random() * SCREEN_HEIGHT);
+// }
+
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
 }
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(gameContainer);
-}
 
-function drawRoad() {
-
-}
-
-function drawCar() {
+function drawAllRoads(roadsArray) {
   
+}
+
+// assume that numLanes is either 1 or an even number
+function drawRoad(point1, point2, numLanes) {
+  if (numLanes == 1) {
+    var new_road = new PIXI.Graphics();
+  } else {
+
+  }
+}
+
+function drawIntersection() {
+
+}
+
+function drawCar(point) {
+
+}
+
+// run the render loop
+animate();
+
+function animate() {
+
+    thing.clear();
+
+    count += 0.1;
+
+    thing.clear();
+    thing.lineStyle(10, 0xff0000, 1);
+    thing.beginFill(0xffFF00, 0.5);
+
+    thing.moveTo(-120 + Math.sin(count) * 20, -100 + Math.cos(count)* 20);
+    thing.lineTo( 120 + Math.cos(count) * 20, -100 + Math.sin(count)* 20);
+    thing.lineTo( 120 + Math.sin(count) * 20, 100 + Math.cos(count)* 20);
+    thing.lineTo( -120 + Math.cos(count)* 20, 100 + Math.sin(count)* 20);
+    thing.lineTo( -120 + Math.sin(count) * 20, -100 + Math.cos(count)* 20);
+
+    thing.rotation = count * 0.1;
+    renderer.render(stage);
+    requestAnimationFrame( animate );
 }

e5ba7746cc08379b9597df3e9657f8cac4ad5dac
Cheng Peng <cwpeng@uwaterloo.ca>
2015-11-21 18:23:25 -0600

    added json road serialization - hopefully

--- TrafficSim/simulation.cpp
+++ TrafficSim/simulation.cpp
@@ -29,60 +29,51 @@ void Simulation::start_simulation() {
 //        for(Entity *e : entities) {
 //            std::cout << '(' << e->getPosition().first << ", " << e->getPosition().second << ')' << std::endl;
 //        }
-//        
-        Json::Value laneVec(Json::arrayValue);
+      
         
-        int start_x = 0, start_y = 100, end_x = 200, end_y = 100;
+        std::vector<Map::Road*> roads = map2->getRoads();
+        std::vector<Map::Intersection*> intersections = map2->getIntersections();
         
-        Json::Value point;
-        point["points"]["start_x"] = start_x;
-        point["points"]["start_y"] = start_y;
-        point["points"]["end_x"] = end_x;
-        point["points"]["end_y"] = end_y;
+        Json::Value json_road;
+        Json::Value json_road_vec(Json::arrayValue);
         
-        laneVec.append(point);
-        std::cout << laneVec << std::endl;
+        for(Map::Road *r : roads) {
+            std::vector<Map::Road::Lane*> lanes = r->getLanes();
+            
+            Json::Value json_lane;
+            Json::Value json_lane_vec(Json::arrayValue);
+            json_lane["lanes"] = json_lane_vec;
+            
+            for(Map::Road::Lane *l : lanes) {
+                // Add points from lane
+                int start_x, start_y, end_x, end_y;
+                
+                std::pair<float,float> startPt = l->get_starting_pt();
+                std::pair<float,float> endPt = l->get_end_pt();
+                
+                start_x = startPt.first;
+                start_y = startPt.second;
+                end_x = endPt.first;
+                end_y = endPt.second;
+                
+                // Serialize points here
+                Json::Value json_point;
+                
+                json_point["points"]["start_x"] = start_x;
+                json_point["points"]["start_y"] = start_y;
+                json_point["points"]["end_x"] = end_x;
+                json_point["points"]["end_y"] = end_y;
+                
+                json_lane_vec.append(json_point);
+            }
+            
+            json_road_vec.append(json_lane);
+        }
         
-//        
-//        std::vector<Map::Road*> roads = map2->getRoads();
-//        std::vector<Map::Intersection*> intersections = map2->getIntersections();
-//        
-//        Json::Value event;
-//        Json::Value roadVec(Json::arrayValue);
-//        
-//        for(Map::Road *r : roads) {
-//            std::vector<Map::Road::Lane*> lanes = r->getLanes();
-//            
-//            Json::Value laneVec(Json::arrayValue);
-//            event["roads"]["lanes"] = laneVec;
-//            
-//            for(Map::Road::Lane *l : lanes) {
-//                
-//                int start_x, start_y, end_x, end_y;
-        Map::Road::Lane *l;
-        std::pair<float,float> startPt = l->get_starting_pt();
-        std::pair<float,float> endPt = l->get_end_pt();
+        json_road["lanes"] = json_road_vec;
         
-        start_x = startPt.first;
-        start_y = startPt.second;
-        end_x = endPt.first;
-        end_y = endPt.second;
-        
-        
-//                Json::Value point;
-//                point["points"]["start_x"] = start_x;
-//                point["points"]["start_y"] = start_y;
-//                point["points"]["end_x"] = end_x;
-//                point["points"]["end_y"] = end_y;
-//
-//                laneVec.append(point);
-//            }
-//        }
-//        
-//        
-//        event["intersections"] = "";
-        
-    
+        std::cout << json_road << endl;
+
         time_passed += INTERVAL_IN_MS;
     }
 }
